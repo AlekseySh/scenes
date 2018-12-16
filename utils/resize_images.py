73b3e56c27479d0e230b951fd1aace0b3922f60e
AlekseySh <lexibender@ya.ru>
2018-12-16 20:46:46 +0300

    minor

--- utils/resize_images.py
+++ utils/resize_images.py
@@ -6,26 +6,40 @@ import matplotlib.image as mpimg
 from scipy.misc import imresize
 
 
-def resize_and_save(im_path: Path, height: int, width: int):
-    image = mpimg.imread(im_path)
+def resize_and_save(im_path_src: Path,
+                    im_path_dst: Path,
+                    height: int,
+                    width: int
+                    ):
+    image = mpimg.imread(im_path_src)
     image_resized = imresize(arr=image, size=(height, width))
-    mpimg.imsave(im_path, image_resized)
+    mpimg.imsave(im_path_dst, image_resized)
 
 
-def resize_images_recursively(image_dir: Path, height: int, width: int):
-    im_paths = image_dir.glob('**/*.jpg')
-    for path in tqdm(list(im_paths)):
-        resize_and_save(im_path=path, height=height, width=width)
+def resize_images_recursively(src_dir: Path,
+                              dst_dir: Path,
+                              height: int,
+                              width: int
+                              ):
+    im_paths = src_dir.glob('**/*.jpg')
+    for im_path in tqdm(list(im_paths)):
+        resize_and_save(im_path_src=im_path,
+                        im_path_dst=dst_dir / im_path.name,
+                        height=height,
+                        width=width
+                        )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--image_dir', dest='image_dir', type=Path)
+    parser.add_argument('-s', '--src_dir', dest='src_dir', type=Path)
+    parser.add_argument('-d', '--dst_dir', dest='dst_dir', type=Path)
     parser.add_argument('-H', '--height', dest='height', type=int)
     parser.add_argument('-W', '--width', dest='width', type=int)
     args = parser.parse_args()
 
-    resize_images_recursively(image_dir=args.image_dir,
+    resize_images_recursively(src_dir=args.src_dir,
+                              dst_dir=args.dst_dir,
                               height=args.height,
                               width=args.width
                               )
